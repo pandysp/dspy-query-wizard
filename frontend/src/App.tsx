0761bbc636cc9d817d4f9550aac3f9c332210c90
yotam-frid <yotam96@gmail.com>
2025-11-21 20:25:53 +0100

    frontend initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-700 text-white">
+      <div className="">This will be the wizard UI!</div>
+    </div>
+  );
+}
+
+export default App;
